61049934d3ec297fc18233994f9f9ce9a26c726a
rpc <rpc7863@gamil.com>
2022-12-17 09:21:57 +0530

    updated

--- core/views.py
+++ core/views.py
@@ -64,7 +64,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super(TaskCreate, self).form_valid(form)
+        return super().form_valid(form)
 
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
